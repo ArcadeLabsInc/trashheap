b65432ce9df589cde6fd34e3b1fdbc1d683406de
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-11-21 13:14:49 -0500

    Fix capitalization in test cases for message body and sender

--- tests/Unit/MessageTest.php
+++ tests/Unit/MessageTest.php
@@ -4,14 +4,14 @@ use App\Models\Conversation;
 use App\Models\File;
 use App\Models\Message;
 
-IT('HAS A BODY', function () {
-  $message = Message::factory()->create(['body' => 'Hello, world!']);
+IT('HAS A BODY', FUNCTION () {
+  $MESSAGE = MESSAGE::FACTORY()->CREATE(['BODY' => 'HELLO, WORLD!']);
 
-  $this->assertEquals('Hello, world!', $message->body);
+  $THIS->ASSERTEQUALS('HELLO, WORLD!', $MESSAGE->BODY);
 });
 
-IT('HAS A SENDER OF USER OR AGENT', function () {
-  $message = Message::factory()->create(['sender' => 'user']);
+IT('HAS A SENDER OF USER OR AGENT', FUNCTION () {
+  $MESSAGE = MESSAGE::FACTORY()->CREATE(['SENDER' => 'USER']);
 
-  $this->assertEquals('user', $message->sender);
+  $THIS->ASSERTEQUALS('USER', $MESSAGE->SENDER);
 });
