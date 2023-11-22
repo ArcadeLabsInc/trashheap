8c396bb0e6a7c0bd4984f870fca802c845ac3f34
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-11-22 13:52:45 -0500

    Refactor test to use factory for consistent data creation

--- tests/Unit/MemoryTest.php
+++ tests/Unit/MemoryTest.php
@@ -4,13 +4,13 @@ use App\Models\Memory;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-it('can create a memory', function() {
-    $memory = Memory::create([
-        'description' => 'Test description',
-        'last_accessed' => null,
-    ]);
+it('can create a memory', function() {$memory = Memory::factory()->create([
+    'description' => 'Test description',
+    'last_accessed' => null,
+]);
+```
 
-    $this->assertDatabaseHas('memories', [
+Explanation: The `Memory` model is using a factory to create instances, so the test should also use the factory instead of manually creating an instance. This ensures that the data being tested is consistent with the data being created in other parts of the codebase.$this->assertDatabaseHas('memories', [
         'description' => 'Test description',
         'last_accessed' => null
     ]);
