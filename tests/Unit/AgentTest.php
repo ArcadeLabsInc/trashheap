9e6e142be6fe060c67ac7a3bdfdfe688e7970089
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-11-22 13:52:48 -0500

    "Fixed code formatting and added assertions for agent's conversations in Agent model test"

--- tests/Unit/AgentTest.php
+++ tests/Unit/AgentTest.php
@@ -15,21 +15,17 @@ it('belongs to a user', function () {
 it('has a name', function () {
   $agent = Agent::factory()->create(['name' => 'My Agent']);
   expect($agent->name)->toBe('My Agent');
-});
-
-it('has many conversations', function () {
+});it('has many conversations', function () {
   $user = User::factory()->create();
   $agent = Agent::factory()->create(['user_id' => $user->id]);
-  $conversation = Conversation::factory()->create([
+  Conversation::factory()->create([
     'agent_id' => $agent->id,
     'user_id' => $user->id
   ]);
 
-  $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $agent->conversations);
-  $this->assertInstanceOf(Conversation::class, $agent->conversations->first());
-});
-
-it('has many tasks', function () {
+  expect($agent->conversations)->toBeInstanceOf('Illuminate\Database\Eloquent\Collection');
+  expect($agent->conversations[0])->toBeInstanceOf(Conversation::class);
+});it('has many tasks', function () {
   $agent = Agent::factory()->create();
   $task = Task::factory()->create(['agent_id' => $agent->id]);
 
